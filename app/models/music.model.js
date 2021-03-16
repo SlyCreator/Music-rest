5d423b6e2665177853e779825e3ddacaf4f196fe
slycreator <chinonso1amaechi@gmial.com>
2021-03-16 12:30:41 +0100

    added the model for music

---
+++ app/models/music.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("../config/db.config");
+
+const schema = new mongoose.Schema(
+    {
+        title: {
+            desc: "Title of Song",
+            type: String,
+            trim: true,
+            required: true,
+        },
+        genre: {
+            desc: "Genre of music",
+            type: String,
+            trim: true,
+            required: true
+        },
+        duration: {
+            desc: "Duration of song",
+            type: String,
+            trim: true,
+            required: true,
+
+        },
+        year: {
+            desc: "year of production",
+            type: Number,
+            required: true
+        }
+    },
+    { timestamps: { createdAt: "createdAt", updatedAt: "updatedAt" } }
+)
+
+module.exports = mongoose.model("Music",schema)
